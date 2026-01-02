183e2d4264f15bd42231cc94d869e078d119c805
PannnTastic <pannntastic@gmail.com>
2026-01-02 17:08:13 +0700

    feat: Implement DestinasiNavigasi in DestinasiEntry
    Updates the `DestinasiEntry` class to implement the `DestinasiNavigasi` interface.
    
    This change defines the navigation route and title resource for the entry destination:
    - `route` is set to "item_entry".
    - `titleRes` is set to `R.string.entry_siswa`.

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEntry.kt
@@ -1,4 +1,8 @@
 package com.example.myfirebase.view.route
 
-class DestinasiEntry {
+import com.example.myfirebase.R
+
+class DestinasiEntry: DestinasiNavigasi {
+    override val route =  "item_entry"
+    override val titleRes= R.string.entry_siswa
 }
