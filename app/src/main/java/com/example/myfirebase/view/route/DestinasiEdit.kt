d365c3fa064d8ebd3d2e7ea2dde230a04265be1c
PannnTastic <pannntastic@gmail.com>
2026-01-02 17:14:02 +0700

    refactor: Rename itemIdArg to idSiswa in DestinasiEdit
    Updates the `itemIdArg` constant in the `DestinasiEdit.kt` file to `idSiswa`.
    
    This change makes the argument name more specific to the student ID it represents within the navigation route.

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
@@ -5,7 +5,7 @@ import com.example.myfirebase.R
 object DestinasiEdit: DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
-    const val itemIdArg = "itemId"
+    const val itemIdArg = "idSiswa"
     val routeWithArgs = "$route/{$itemIdArg}"
 }
 
