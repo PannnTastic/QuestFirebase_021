a418238fb4f03b55f663247cd0d4c423499c0ea6
PannnTastic <pannntastic@gmail.com>
2026-01-02 17:09:48 +0700

    feat: Add DestinasiHome navigation destination
    Adds the `DestinasiHome` class to define the home screen's navigation destination.
    
    - This class implements the `DestinasiNavigasi` interface.
    - It sets the route to "home" and the title resource to `R.string.app_name`.

---
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.myfirebase.view.route
+
+import com.example.myfirebase.R
+
+class DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes= R.string.app_name
+}
